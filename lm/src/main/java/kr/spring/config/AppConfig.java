6a9de6601cfd166f870dea1e39c9dc8fec499ffc
mona-et <kch990818@daum.net>
2023-07-26 17:50:07 +0900

    first up

---
+++ lm/src/main/java/kr/spring/config/AppConfig.java
@@ -0,0 +1,41 @@
+package kr.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+//자바 코드 기반 설정 클래스
+@Configuration
+public class AppConfig implements WebMvcConfigurer{
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		final TilesConfigurer configurer = new TilesConfigurer();
+		//해당 경로에 xml 설정 파일을 넣음
+		configurer.setDefinitions(new String[] {
+				"/WEB-INF/tiles-def/main.xml",
+				"/WEB-INF/tiles-def/EESAMSAOH.xml",
+				"/WEB-INF/tiles-def/favau.xml",
+				"/WEB-INF/tiles-def/jeongho.xml",
+				"/WEB-INF/tiles-def/jihye.xml",
+				"/WEB-INF/tiles-def/mona.xml",
+				"/WEB-INF/tiles-def/msb.xml",
+				"/WEB-INF/tiles-def/wjswkdrms.xml"
+		});
+		return configurer;
+	}
+	@Bean
+	public TilesViewResolver tilesViewResolver() {
+		final TilesViewResolver tilesViewResolver = 
+				                    new TilesViewResolver();
+		tilesViewResolver.setViewClass(TilesView.class);
+		return tilesViewResolver;
+	}
+}
+
+
+
