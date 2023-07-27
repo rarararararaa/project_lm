085e39e19970a9225e4b6b44b8a1f39c4875f68d
jeongho.park1993 <jeongho.park1993@gmail.com>
2023-07-27 15:17:52 +0900

    fix

--- lm/src/main/java/kr/spring/config/AppConfig.java
+++ lm/src/main/java/kr/spring/config/AppConfig.java
@@ -18,7 +18,14 @@ public class AppConfig implements WebMvcConfigurer{
 		//해당 경로에 xml 설정 파일을 넣음
 		configurer.setDefinitions(new String[] {
 				"/WEB-INF/tiles-def/bookstoreMain.xml",
-				"/WEB-INF/tiles-def/libMain.xml"
+				"/WEB-INF/tiles-def/libMain.xml",
+				"/WEB-INF/tiles-def/EESAMSAOH.xml",
+				"/WEB-INF/tiles-def/favau.xml",
+				"/WEB-INF/tiles-def/jeongho.xml",
+				"/WEB-INF/tiles-def/jihye.xml",
+				"/WEB-INF/tiles-def/mona.xml",
+				"/WEB-INF/tiles-def/msb.xml",
+				"/WEB-INF/tiles-def/wjswkdrms.xml"
 				
 		});
 		return configurer;
