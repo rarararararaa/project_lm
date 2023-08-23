4d68b89232ddde1fd5699dcae110043bd41bb07b
gpw431 <gpw431@gmail.com>
2023-08-23 14:04:15 +0900

    베스트 도서 완료

--- lm/src/main/resources/static/js/productBest.js
+++ lm/src/main/resources/static/js/productBest.js
@@ -62,18 +62,17 @@ $(function(){
 		}
 	})
 	//분야별
-		new Swiper('.category', {
+	new Swiper('.cate-best-swiper', {
 	  //initialSlide: -2,//슬라이드 시작 지점
 	  direction: 'vertical',//수직
 	  autoplay: {//자동 슬라이드
-	    delay: 1000,
+	    delay: 4000,
 		disableOnInteraction: false
 	  },
 	  loop: true,//슬라이드 반복 여부
-	//  loopAdditionalSlides : 1,// 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
-	  slidesPerView: 5,//한 슬라이드에 보여줄 갯수
-	  spaceBetween: 1,//슬라이드 사이의 공맥
-	  centeredSlides: true, //센터모드
+	  //loopAdditionalSlides : 1,// 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
+	  //spaceBetween: 10,//슬라이드 사이의 공맥
+	  //centeredSlides: true, //센터모드
 /*	  pagination: {//호출(pager)여부
 	    el: '.swiper-pagination',//버튼을 담을 태그 설정
 	    clickable: true //버튼 클릭 여부
@@ -82,7 +81,7 @@ $(function(){
 	    prevEl: '.cate-prev',
 	    nextEl: '.cate-next'
 	  },
-		autoHeight:false, //현재 활성 슬라이드 뫂이에 맞게 조정
+		//autoHeight:false, //현재 활성 슬라이드 뫂이에 맞게 조정
 	})
 
 })
